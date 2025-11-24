521a70b1ba979d10796cee6d2b4d406596fef180
bitbonk <bitbonk@msn.com>
2025-11-24 13:06:24 +0100

    Add more info

--- Libs/MessagePackNotFound.CommonLib/Dto.cs
+++ Libs/MessagePackNotFound.CommonLib/Dto.cs
@@ -5,7 +5,7 @@ namespace MessagePackNotFound.CommonLib;
 /// <summary>
 ///     Some DTO with some MessagePack customization that will be serialized and deserialized.
 /// </summary>
-// [MessagePackObject(AllowPrivate = true)] // TODO this attribute will trigger source generation which then breaks the build
+[MessagePackObject(AllowPrivate = true)] // TODO this attribute will trigger source generation which then breaks the build
 internal class Dto
 {
     public Dto()
